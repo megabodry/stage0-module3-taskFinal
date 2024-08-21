08541a67805493d7972d38b9751a563c94c66c91
Dzmitry Bodry <megabodry@gmail.com>
2024-08-21 15:11:30 +0300

    Completed Task Final.

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number % 10;
+        number /= 10;
+        int second = number % 10;
+        number /= 10;
+        int third = number % 10;
+        number /= 10;
+        int fourth = number % 10;
+        System.out.println(first + second + third + fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number % 10;
+        number /= 10;
+        int second = number % 10;
+        number /= 10;
+        int third = number % 10;
+        System.out.println(first * 100 + second * 10 + third);
     }
 }
